9523b3ac5d04dbefbae31e21d4cf0a487f6cbe12
LahevOdVika <75607884+LahevOdVika@users.noreply.github.com>
2024-10-13 22:30:57 +0200

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,17 +7,12 @@ import Image from 'next/image'
 
 export default function Page() {
 
-    let firstRef;
-    let secondRef;
-    let thirdRef;
+    const firstRef = useRef(null);
+    const secondRef = useRef(null);
+    const thirdRef = useRef(null);
 
-    if (!window.matchMedia('prefers-reduced-motion').matches) {
-
-        firstRef = useRef(null);
-        secondRef = useRef(null);
-        thirdRef = useRef(null);
-
-        useEffect(() => {
+    useEffect(() => {
+        if (typeof window !== 'undefined' && !window.matchMedia('prefers-reduced-motion').matches) {
             const observer = new IntersectionObserver((entities) => {
                 entities.forEach(entity => {
                     if (entity.isIntersecting) {
@@ -29,12 +24,12 @@ export default function Page() {
                             entity.target.classList.add(styles.animate);
                         }
                     }
-                })
+                });
             }, {
                 root: null,
                 rootMargin: '0px',
                 threshold: 0.1
-            })
+            });
 
             if (firstRef.current) observer.observe(firstRef.current);
             if (secondRef.current) observer.observe(secondRef.current);
@@ -45,78 +40,78 @@ export default function Page() {
                 if (secondRef.current) observer.unobserve(secondRef.current);
                 if (thirdRef.current) observer.unobserve(thirdRef.current);
             };
-        }, []);
-    }
+        }
+    }, []);
 
     return <>
-            <main className={styles.welcome}>
-                <div className="container">
-                    <h1>Dear diary</h1>
-                    <h2>I'm a junior "full stack" web, desktop and mobile developer</h2>
+        <main className={styles.welcome}>
+            <div className="container">
+                <h1>Dear diary</h1>
+                <h2>I'm a junior "full stack" web, desktop and mobile developer</h2>
+            </div>
+            <Image
+                src="/image.png"
+                alt="Welcome image"
+                width={500}
+                height={500}
+            />
+        </main>
+        <div className={styles.cards}>
+            <div className={styles.cardSlider}>
+                <Card imageUrl={"https://studio-am.cz/favicon.ico"} imageAlt={"Placeholder"} header={"Card 1"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 2"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 3"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 4"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 5"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 6"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+            </div>
+            <div className={styles.cardSlider}>
+                <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 1"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 2"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 3"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 4"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 5"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+                <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 6"}
+                      text={"Lorem ipsum dolor sit amet."}></Card>
+            </div>
+        </div>
+        <section>
+            <div className={styles.timelineSection} ref={secondRef}>
+                <div className={styles.timelinePart}>
+                    <img src="/timeline/Code.svg" alt="Code"/>
+                    <img src="/timeline/LineMiddle.svg" alt="Line"/>
                 </div>
-                <Image
-                    src="/image.png"
-                    alt="Welcome image"
-                    width={500}
-                    height={500}
-                />
-            </main>
-            <div className={styles.cards}>
-                <div className={styles.cardSlider}>
-                    <Card imageUrl={"https://studio-am.cz/favicon.ico"} imageAlt={"Placeholder"} header={"Card 1"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 2"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 3"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 4"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 5"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 6"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                </div>
-                <div className={styles.cardSlider}>
-                    <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 1"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 2"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 3"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 4"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/300"} imageAlt={"Placeholder"} header={"Card 5"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
-                    <Card imageUrl={"https://placehold.co/200"} imageAlt={"Placeholder"} header={"Card 6"}
-                          text={"Lorem ipsum dolor sit amet."}></Card>
+                <div className={styles.timelineContent}>
+                    <img src="/timeline/Langs.svg" alt="Langs"/>
+                    I code everything myself, no website builders, no AI builders
                 </div>
             </div>
-            <section>
-                <div className={styles.timelineSection} ref={secondRef}>
-                    <div className={styles.timelinePart}>
-                        <img src="/timeline/Code.svg" alt="Code"/>
-                        <img src="/timeline/LineMiddle.svg" alt="Line"/>
-                    </div>
-                    <div className={styles.timelineContent}>
-                        <img src="/timeline/Langs.svg" alt="Langs"/>
-                        I code everything myself, no website builders, no AI builders
-                    </div>
+            <div className={styles.timelineSection} ref={thirdRef}>
+                <div className={styles.timelinePart}>
+                    <img src="/timeline/Terminal.svg" alt="Terminal"/>
+                    <img src="/timeline/LineBottom.svg" alt="Line"/>
                 </div>
-                <div className={styles.timelineSection} ref={thirdRef}>
-                    <div className={styles.timelinePart}>
-                        <img src="/timeline/Terminal.svg" alt="Terminal"/>
-                        <img src="/timeline/LineBottom.svg" alt="Line"/>
-                    </div>
-                    <div className={styles.timelineContent}>
-                        <Image
-                            src="/timeline/Utils.svg"
-                            alt="Programs I use to code"
-                            width={502}
-                            height={470}
-                        />
-                        Using only the best tools on the market
-                    </div>
+                <div className={styles.timelineContent}>
+                    <Image
+                        src="/timeline/Utils.svg"
+                        alt="Programs I use to code"
+                        width={502}
+                        height={470}
+                    />
+                    Using only the best tools on the market
                 </div>
-            </section>
+            </div>
+        </section>
     </>
 }

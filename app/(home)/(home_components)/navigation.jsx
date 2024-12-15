9f4a04bbf1759f6873b222899cb40e51624f9977
LahevOdVika <michalBudai@outlook.cz>
2024-12-15 19:54:00 +0100

    Improved navigation bar

--- app/(home)/(home_components)/navigation.jsx
+++ app/(home)/(home_components)/navigation.jsx
@@ -1,41 +1,52 @@
-"use client";
+'use client';
 
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import Link from "next/link";
-import {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 import * as icon from '@fortawesome/free-solid-svg-icons';
 
 export default function Navigation() {
 
-    const [isOpened, setIsOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
+  const menuRef = useRef(null);
 
-    const handleClick = () => {
-        setIsOpened(!isOpened);
+  const handleClick = () => {
+    setIsOpened(!isOpened);
+  };
+
+  const handleClose = () => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsOpened(false);
     }
+  }
 
-    return <nav>
-            <button type="button" className={"faBars"} onClick={handleClick}>
-                <FontAwesomeIcon icon={icon.faBars} />
-            </button>
-            <ul className={"menu"} data-opened={isOpened}>
-                <li onClick={handleClick}>
-                  <Link href="/">
-                    <FontAwesomeIcon icon={icon.faHouse} style={{marginRight: '10px'}} />
-                    Home
-                  </Link>
-                </li>
-                <li onClick={handleClick}>
-                  <Link href="/">
-                    <FontAwesomeIcon icon={icon.faSuitcase} style={{marginRight: '10px'}} />
-                    My Work
-                  </Link>
-                </li>
-                <li onClick={handleClick}>
-                  <Link href="/contact">
-                    <FontAwesomeIcon icon={icon.faPhone} style={{marginRight: '10px'}} />
-                    Contact
-                  </Link>
-                </li>
-            </ul>
-        </nav>
+  useEffect(() => {
+    document.addEventListener('click', handleClose);
+  });
+
+  return <nav ref={menuRef}>
+    <button type="button" className={'faBars'} onClick={handleClick}>
+      <FontAwesomeIcon icon={icon.faBars} />
+    </button>
+    <ul className={'menu'} data-opened={isOpened}>
+      <li onClick={handleClick}>
+        <Link href="/">
+          <FontAwesomeIcon icon={icon.faHouse} style={{ marginRight: '10px' }} />
+          Home
+        </Link>
+      </li>
+      <li onClick={handleClick}>
+        <Link href="/">
+          <FontAwesomeIcon icon={icon.faSuitcase} style={{ marginRight: '10px' }} />
+          My Work
+        </Link>
+      </li>
+      <li onClick={handleClick}>
+        <Link href="/contact">
+          <FontAwesomeIcon icon={icon.faPhone} style={{ marginRight: '10px' }} />
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>;
 }
--- app/layout.jsx
+++ app/layout.jsx
@@ -8,22 +8,20 @@ export const metadata = {
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import ScrollListener from '@/app/(home)/(home_components)/ScrollListener';
 config.autoAddCss = false
 
-
-
 export default function RootLayout({children}) {
-    return <html lang={"en"}>
-            <body>
-            <header>
-                <div className="copyright">
-                    &copy; 2024 Michal Budai
-                </div>
-                <Navigation />
-            </header>
-            <div className="wrapper">
-              {children}
-            </div>
-            </body>
-        </html>
+
+  return <html lang={"en"}>
+          <body>
+          <header>
+              <Navigation />
+          </header>
+          <div className="wrapper">
+            <ScrollListener />
+            {children}
+          </div>
+          </body>
+      </html>
 }
